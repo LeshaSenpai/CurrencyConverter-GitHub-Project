6f946d686125991f9466ffec2b7576b3706064b0
LeshaSenpai <134790759+LeshaSenpai@users.noreply.github.com>
2024-09-10 12:26:41 +0300

    clean project

--- dist/pages/NotFound.js
+++
@@ -1,4 +0,0 @@
-function NotFound() {
-    return React.createElement("h1", null, "\u042D\u0442\u0430 \u0441\u0442\u0440\u0430\u043D\u0438\u0446\u0430 \u043D\u0435 \u0441\u0443\u0449\u0435\u0441\u0442\u0432\u0443\u0435\u0442");
-}
-export { NotFound };
